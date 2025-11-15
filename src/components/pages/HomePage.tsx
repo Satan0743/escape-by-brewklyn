ffc7f6b4dc0254f951a9d8c1c73f4dfd7bfaf067
Wix Remote Machine <support@wix.com>
2025-11-15 16:16:11 +0000

    Replace Saison Image

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -587,6 +587,8 @@ Please contact the customer to confirm their reservation.
                           ? "https://static.wixstatic.com/media/4d5d5f_6ecdd848d1954a64a9ce82603b2ebe14~mv2.jpg"
                           : brewName === 'Mango wheat'
                           ? "https://static.wixstatic.com/media/4d5d5f_965a8a4a32fd49b58d1fdd261493c87f~mv2.jpg"
+                          : brewName === 'Saison'
+                          ? "https://static.wixstatic.com/media/4d5d5f_0608022f2855411a80362cfbbe9e63f8~mv2.webp"
                           : "https://static.wixstatic.com/media/4d5d5f_531895c85ac24c06be160a524c126a6e~mv2.png?originWidth=384&originHeight=256"
                       }
                       alt={brewName}
