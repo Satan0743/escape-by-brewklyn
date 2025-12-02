62d3d79d7effa58249f80909bfa7ff4c6d5ef9cc
Wix Remote Machine <support@wix.com>
2025-12-02 12:02:39 +0000

    Published

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -181,7 +181,7 @@ Please contact the customer to confirm their reservation.
             >
               <Image
                 src="https://static.wixstatic.com/media/4d5d5f_f5ac964b85534ad6850fa320f3b6c1ac~mv2.jpg"
-                className="h-12 w-auto border-[0px] border-[#ebe5e8] border-solid rounded-[30px] shadow-[inset_0px_0px_4px_0px_#bfbfbf] -ml-2"
+                className="h-12 w-auto border-[0px] border-[#ebe5e8] border-solid shadow-[inset_0px_0px_4px_0px_#bfbfbf] -ml-2 rounded-[49px]"
                 width={200}
                 alt="Escape by Brewklyn logo" />
             </motion.div>
